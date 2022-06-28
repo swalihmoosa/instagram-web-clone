d96fe2a37631358b9bccd5c740b8192fde272992
swalihmoosa <swalihmoosa72@gmail.com>
2022-06-28 18:18:36 +0530

    ExploreScreen | ongoing

--- src/components/screens/ExploreScreen.jsx
+++ src/components/screens/ExploreScreen.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import styled from 'styled-components';
 
 
 export default function ExploreScreen() {
     return (
-        <div>ExploreScreen</div>
+        <Container>
+            <section className="wrapper">
+                <ExploreUl>
+                    <ExploreLi>
+                        <img src='' alt='Explore' />
+                    </ExploreLi>
+                </ExploreUl>
+            </section>
+        </Container>
     )
 }
+const Container = styled.section`
+    background-color: #fafafa;
+    & .wrapper {
+        padding: 115px 50px;
+        display: flex;
+        justify-content: space-between;
+        position: relative;
+    }
+`;
+const ExploreUl = styled.div``
+const ExploreLi = styled.div``
