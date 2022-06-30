a86442dafe0009fac784f3f518498db68023e418
swalihmoosa <swalihmoosa72@gmail.com>
2022-06-30 15:04:46 +0530

    explores.jsx | created

--- src/assets/json/explores.js
+++ src/assets/json/explores.js
@@ -5,7 +5,20 @@ const explores = [
         "name": "Anshab Ansh",
         "profile_image": "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1600",
         "thumbnail_image": "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        "explore_data" : "",
+        "explore_data" : [
+            {
+                id:1,
+                data : "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            },
+            {
+                id:1,
+                data : "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            },
+            {
+                id:1,
+                data : "https://images.pexels.com/photos/943084/pexels-photo-943084.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            },
+        ],
     },
     {
         "id": 2,
@@ -13,7 +26,7 @@ const explores = [
         "name": "Anshab Ansh",
         "profile_image": "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1600",
         "thumbnail_image": "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        "explore_data" : "",
+        "explore_data" : "https://www.youtube.com/watch?v=ysz5S6PUM-U",
     },
     {
         "id": 3,
@@ -21,7 +34,7 @@ const explores = [
         "name": "Anshab Ansh",
         "profile_image": "https://images.pexels.com/photos/943084/pexels-photo-943084.jpeg?auto=compress&cs=tinysrgb&w=1600",
         "thumbnail_image": "https://images.pexels.com/photos/943084/pexels-photo-943084.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        "explore_data" : "",
+        "explore_data" : "https://youtu.be/oQjdSf0tlAc",
     },
     {
         "id": 4,
@@ -29,7 +42,16 @@ const explores = [
         "name": "Anshab Ansh",
         "profile_image": "https://images.pexels.com/photos/1542085/pexels-photo-1542085.jpeg?auto=compress&cs=tinysrgb&w=1600",
         "thumbnail_image": "https://images.pexels.com/photos/1542085/pexels-photo-1542085.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        "explore_data" : "",
+        "explore_data" : [
+            {
+                id:1,
+                data : "https://youtu.be/ZQsI6m66Xdo"
+            },
+            {
+                id:1,
+                data : "https://youtu.be/x0MwXUNhXKc"
+            },
+        ],
     },
     {
         "id": 5,
@@ -37,7 +59,7 @@ const explores = [
         "name": "Anshab Ansh",
         "profile_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU4g8xUnnDU4kVOp8_-3f3aPDusw_D2AlyXw&usqp=CAU",
         "thumbnail_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU4g8xUnnDU4kVOp8_-3f3aPDusw_D2AlyXw&usqp=CAU",
-        "explore_data" : "",
+        "explore_data" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuXK9aErkNasw27BhsCRQwa1LurjFCwNjwnQ&usqp=CAU",
     },
     {
         "id": 6,
@@ -45,7 +67,7 @@ const explores = [
         "name": "Anshab Ansh",
         "profile_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuXK9aErkNasw27BhsCRQwa1LurjFCwNjwnQ&usqp=CAU",
         "thumbnail_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuXK9aErkNasw27BhsCRQwa1LurjFCwNjwnQ&usqp=CAU",
-        "explore_data" : "",
+        "explore_data" : "https://youtu.be/ek2XsdApXQc",
     },
     {
         "id": 7,
@@ -53,7 +75,7 @@ const explores = [
         "name": "Anshab Ansh",
         "profile_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkspTGDaYI0SloxfqGWTJMZYniyE8q9oqahw&usqp=CAU",
         "thumbnail_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkspTGDaYI0SloxfqGWTJMZYniyE8q9oqahw&usqp=CAU",
-        "explore_data" : "",
+        "explore_data" : "https://youtu.be/VCvwy4dFUmw",
     },
     {
         "id": 8,
@@ -61,7 +83,20 @@ const explores = [
         "name": "Anshab Ansh",
         "profile_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNeAcbGLYtVaSSR9SaiRBAmxr94SxDIkHKeQ&usqp=CAU",
         "thumbnail_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNeAcbGLYtVaSSR9SaiRBAmxr94SxDIkHKeQ&usqp=CAU",
-        "explore_data" : "",
+        "explore_data" : [
+            {
+                id:1,
+                data:"https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBwaG90b3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+            },
+            {
+                id:2,
+                data:"https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGQlMjBwaG90b3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+            },
+            {
+                id:3,
+                data:"https://images.unsplash.com/photo-1615871154954-3a50b273dbf6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aGQlMjBwaG90b3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+            },
+        ],
     },
     {
         "id": 9,
@@ -69,7 +104,20 @@ const explores = [
         "name": "Anshab Ansh",
         "profile_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQAMSOmvBpTbwTM2oDxWfeW1KXqpsSzlOKxQ&usqp=CAU",
         "thumbnail_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQAMSOmvBpTbwTM2oDxWfeW1KXqpsSzlOKxQ&usqp=CAU",
-        "explore_data" : "",
+        "explore_data" : [
+            {
+                id:1,
+                data:"https://images.unsplash.com/photo-1611316185995-9624c94487d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGhkJTIwcGhvdG9zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            },
+            {
+                id:2,
+                data:"https://images.unsplash.com/photo-1571348264903-c3af9582e4ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGhkJTIwcGhvdG9zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            },
+            {
+                id:3,
+                data:"https://youtu.be/6ifngyPjQ3U"
+            },
+        ],
     },
     {
         "id": 10,
@@ -77,7 +125,20 @@ const explores = [
         "name": "Anshab Ansh",
         "profile_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPgQ3BUZPAQeJgWbu88weHk4X7rNTrcWgBSw&usqp=CAU",
         "thumbnail_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPgQ3BUZPAQeJgWbu88weHk4X7rNTrcWgBSw&usqp=CAU",
-        "explore_data" : "",
+                "explore_data" : [
+            {
+                id:1,
+                data:"https://images.unsplash.com/photo-1571348264903-c3af9582e4ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGhkJTIwcGhvdG9zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            },
+            {
+                id:2,
+                data:"https://youtu.be/tXlhi0oXKiE"
+            },
+            {
+                id:3,
+                data:"https://youtu.be/Y9srFJwojGA"
+            },
+        ],
     },
     {
         "id": 11,
@@ -85,7 +146,20 @@ const explores = [
         "name": "Anshab Ansh",
         "profile_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7njl1_cq_4eXYQlyaSvQM7eiionn-35yupg&usqp=CAU",
         "thumbnail_image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7njl1_cq_4eXYQlyaSvQM7eiionn-35yupg&usqp=CAU",
-        "explore_data" : "",
+                "explore_data" : [
+            {
+                id:1,
+                data:"https://images.unsplash.com/photo-1614160859544-177611d11f6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aGQlMjBwaG90b3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+            },
+            {
+                id:2,
+                data:"https://youtu.be/FU_LrZydhBE"
+            },
+            {
+                id:3,
+                data:"https://youtu.be/WzKhvsUoXeI"
+            },
+        ],
     },
     {
         "id": 12,
@@ -93,7 +167,20 @@ const explores = [
         "name": "Anshab Ansh",
         "profile_image": "https://cdn.mos.cms.futurecdn.net/CAZ6JXi6huSuN4QGE627NR-970-80.jpg.webp",
         "thumbnail_image": "https://cdn.mos.cms.futurecdn.net/CAZ6JXi6huSuN4QGE627NR-970-80.jpg.webp",
-        "explore_data" : "",
+                "explore_data" : [
+            {
+                id:1,
+                data:"https://images.unsplash.com/photo-1614160859544-177611d11f6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aGQlMjBwaG90b3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+            },
+            {
+                id:2,
+                data:"https://images.unsplash.com/photo-1541411438265-4cb4687110f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aGQlMjBwaG90b3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+            },
+            {
+                id:3,
+                data:"https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aGQlMjBwaG90b3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+            },
+        ],
     },
 ]
 
