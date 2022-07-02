6a03672904cf52ea74c6dba31e1f8b8feea72c42
swalihmoosa <swalihmoosa72@gmail.com>
2022-07-02 18:21:44 +0530

    ActivitiesModal | completed

--- src/assets/json/stories.js
+++ src/assets/json/stories.js
@@ -1,8 +1,8 @@
 const stories = [
     {
         id: 1,
-        username: "junior_moosa",
-        name: "Swalih Moosa",
+        username: "aswathy_achu",
+        name: "Aswathy",
         story: "https://cdn.mos.cms.futurecdn.net/CAZ6JXi6huSuN4QGE627NR-970-80.jpg.webp",
         profile_image:
             "https://cdn.mos.cms.futurecdn.net/CAZ6JXi6huSuN4QGE627NR-970-80.jpg.webp",
@@ -16,7 +16,7 @@ const stories = [
     },
     {
         id: 3,
-        username: "lallu",
+        username: "__lallu__",
         name: "Naveen",
         story: "https://thumbs.dreamstime.com/z/woman-praying-free-birds-to-nature-sunset-background-woman-praying-free-birds-enjoying-nature-sunset-99680945.jpg",
         profile_image:
@@ -24,23 +24,23 @@ const stories = [
     },
     {
         id: 4,
-        username: "albin_royson",
-        name: "Swalih Moosa",
+        username: "afsal_salim",
+        name: "Afsal Salim",
         story: "https://thumbs.dreamstime.com/b/free-happy-woman-praising-freedom-beach-sunset-arms-up-young-adult-enjoying-breathing-freely-fresh-air-49157481.jpg",
         profile_image:
             "https://thumbs.dreamstime.com/b/free-happy-woman-praising-freedom-beach-sunset-arms-up-young-adult-enjoying-breathing-freely-fresh-air-49157481.jpg",
     },
     {
         id: 5,
-        username: "albin_royson",
-        name: "Swalih Moosa",
+        username: "moto_rider",
+        name: "Raju jacob",
         story: "https://thumbs.dreamstime.com/b/free-thinking-nourish-your-mind-positive-thoughts-good-intentions-brain-power-concept-woman-head-silhouette-sunset-inside-157807698.jpg",
         profile_image:
             "https://thumbs.dreamstime.com/b/free-thinking-nourish-your-mind-positive-thoughts-good-intentions-brain-power-concept-woman-head-silhouette-sunset-inside-157807698.jpg",
     },
     {
         id: 6,
-        username: "albin_royson",
+        username: "swalih_moosa",
         name: "Swalih Moosa",
         story: "https://thumbs.dreamstime.com/b/business-woman-relaxing-sitting-office-stress-free-time-management-concept-happy-young-her-65461487.jpg",
         profile_image:
@@ -48,40 +48,40 @@ const stories = [
     },
     {
         id: 7,
-        username: "albin_royson",
-        name: "Swalih Moosa",
+        username: "kalidas_",
+        name: "Kalidas",
         story: "https://thumbs.dreamstime.com/b/free-range-chickens-farm-outdoor-91178223.jpg",
         profile_image:
             "https://thumbs.dreamstime.com/b/free-range-chickens-farm-outdoor-91178223.jpg",
     },
     {
         id: 8,
-        username: "albin_royson",
-        name: "Swalih Moosa",
+        username: "rocky_bhai",
+        name: "Rocky",
         story: "https://thumbs.dreamstime.com/b/freedom-people-living-free-happy-life-beach-carefree-silhouettes-couple-sunset-arms-raised-up-showing-happiness-52124196.jpg",
         profile_image:
             "https://thumbs.dreamstime.com/b/freedom-people-living-free-happy-life-beach-carefree-silhouettes-couple-sunset-arms-raised-up-showing-happiness-52124196.jpg",
     },
     {
         id: 9,
-        username: "albin_royson",
-        name: "Swalih Moosa",
+        username: "loki__",
+        name: "Loki",
         story: "https://thumbs.dreamstime.com/b/woman-feeling-free-beautiful-natural-setting-what-field-sunset-chains-175907923.jpg",
         profile_image:
             "https://thumbs.dreamstime.com/b/woman-feeling-free-beautiful-natural-setting-what-field-sunset-chains-175907923.jpg",
     },
     {
         id: 10,
-        username: "albin_royson",
-        name: "Swalih Moosa",
+        username: "__marvel__",
+        name: "Marvel",
         story: "https://thumbs.dreamstime.com/b/free-as-wind-5565107.jpg",
         profile_image:
             "https://thumbs.dreamstime.com/b/free-as-wind-5565107.jpg",
     },
     {
         id: 11,
-        username: "albin_royson",
-        name: "Swalih Moosa",
+        username: "_lucky_",
+        name: "Lucky",
         story: "https://thumbs.dreamstime.com/z/woman-praying-free-birds-to-nature-sunset-background-woman-praying-free-birds-enjoying-nature-sunset-99680945.jpg",
         profile_image:
             "https://thumbs.dreamstime.com/z/woman-praying-free-birds-to-nature-sunset-background-woman-praying-free-birds-enjoying-nature-sunset-99680945.jpg",
