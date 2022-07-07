52c0819ecb415750723fe66cd20de62a45872b09
swalihmoosa <swalihmoosa72@gmail.com>
2022-07-07 16:42:47 +0530

    userData.js | ongoing

---
+++ src/assets/json/userData.js
@@ -0,0 +1,40 @@
+const userData = {
+    username : "junior_moosa",
+    name : "Swalih Moosa",
+    followers : [
+        {
+            username : "abu",
+            name : "Abu",
+            avatar : "",
+        },
+        {
+            username : "david",
+            name : "David",
+            avatar : "",
+        },
+        {
+            username : "varsha",
+            name : "Varsha",
+            avatar : "",
+        },
+    ],
+    following : [
+        {
+            username : "abu",
+            name : "Abu",
+            avatar : "",
+        },
+        {
+            username : "david",
+            name : "David",
+            avatar : "",
+        },
+        {
+            username : "varsha",
+            name : "Varsha",
+            avatar : "",
+        },
+    ]
+}
+
+export default userData;
