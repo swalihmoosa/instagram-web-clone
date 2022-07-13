547e7e083d282355958015882e597910bd05393e
swalihmoosa <swalihmoosa72@gmail.com>
2022-07-13 17:15:43 +0530

    userData | ongoing

--- src/assets/json/userData.js
+++ src/assets/json/userData.js
@@ -1,40 +1,310 @@
+import postOne from "../images/my-posts/junior-moosa.jpg";
+
 const userData = {
-    username : "junior_moosa",
-    name : "Swalih Moosa",
-    followers : [
+    username: "junior_moosa",
+    name: "Swalih Moosa",
+    followers: [
         {
-            username : "abu",
-            name : "Abu",
-            avatar : "",
+            username: "abu",
+            name: "Abu",
+            avatar: "",
         },
         {
-            username : "david",
-            name : "David",
-            avatar : "",
+            username: "david",
+            name: "David",
+            avatar: "",
         },
         {
-            username : "varsha",
-            name : "Varsha",
-            avatar : "",
+            username: "varsha",
+            name: "Varsha",
+            avatar: "",
         },
     ],
-    following : [
+    following: [
         {
-            username : "abu",
-            name : "Abu",
-            avatar : "",
+            username: "abu",
+            name: "Abu",
+            avatar: "",
         },
         {
-            username : "david",
-            name : "David",
-            avatar : "",
+            username: "david",
+            name: "David",
+            avatar: "",
         },
         {
-            username : "varsha",
-            name : "Varsha",
-            avatar : "",
+            username: "varsha",
+            name: "Varsha",
+            avatar: "",
         },
-    ]
-}
+    ],
+    company: "Steyp",
+    bio: `Software Engineer @${company} \\n Peacefull Life \\n Everyone Who Starts has a Dream about the End 💫 \\n Junior Moosa ✌️ \\n MuziC 🎧
+    `,
+    highlights: [
+        {
+            id: 1,
+            title: "Nature",
+            stories: [
+                {
+                    id: 1,
+                    story: "https://images.unsplash.com/photo-1611782712936-6247d9711816?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dWVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+                },
+                {
+                    id: 2,
+                    story: "https://images.unsplash.com/photo-1535000367458-13b83fad46f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG5hdHVlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+                },
+                {
+                    id: 3,
+                    story: "https://images.unsplash.com/photo-1654543876625-d923eff049eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fG5hdHVlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+                },
+                {
+                    id: 4,
+                    story: "https://images.unsplash.com/photo-1651945515343-2b23ccfbfce0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG5hdHVlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+                },
+                {
+                    id: 5,
+                    story: "https://media.istockphoto.com/photos/flower-in-mist-picture-id180478956?b=1&k=20&m=180478956&s=170667a&w=0&h=ZbcE-bkXJnQDt2Eyd1xy-PXt9lrV81SPi3khKv_5MRM=",
+                },
+            ],
+        },
+        {
+            id: 2,
+            title: "Friends",
+            stories: [
+                {
+                    id: 1,
+                    story: "https://www.whatsappimages.in/wp-content/uploads/2021/06/Free-Friends-Group-Images-Pics-Download-for-Whatsapp.jpg",
+                },
+                {
+                    id: 2,
+                    story: "https://images.pexels.com/photos/745045/pexels-photo-745045.jpeg?auto=compress&cs=tinysrgb&w=1600",
+                },
+                {
+                    id: 3,
+                    story: "https://images.pexels.com/photos/1255062/pexels-photo-1255062.jpeg?auto=compress&cs=tinysrgb&w=1600",
+                },
+                {
+                    id: 4,
+                    story: "https://images.pexels.com/photos/1036804/pexels-photo-1036804.jpeg?auto=compress&cs=tinysrgb&w=1600",
+                },
+                {
+                    id: 5,
+                    story: "https://images.pexels.com/photos/1524105/pexels-photo-1524105.jpeg?auto=compress&cs=tinysrgb&w=1600",
+                },
+                {
+                    id: 6,
+                    story: "https://images.pexels.com/photos/708440/pexels-photo-708440.jpeg?auto=compress&cs=tinysrgb&w=1600",
+                },
+            ],
+        },
+        {
+            id: 3,
+            title: "Hills",
+            stories: [
+                {
+                    id: 1,
+                    story: "https://images.pexels.com/photos/705075/pexels-photo-705075.jpeg?auto=compress&cs=tinysrgb&w=1600",
+                },
+                {
+                    id: 2,
+                    story: "https://images.pexels.com/photos/1658967/pexels-photo-1658967.jpeg?auto=compress&cs=tinysrgb&w=1600",
+                },
+                {
+                    id: 3,
+                    story: "https://images.pexels.com/photos/803105/pexels-photo-803105.jpeg?auto=compress&cs=tinysrgb&w=1600",
+                },
+                {
+                    id: 4,
+                    story: "https://images.pexels.com/photos/1587699/pexels-photo-1587699.jpeg?auto=compress&cs=tinysrgb&w=1600",
+                },
+                {
+                    id: 5,
+                    story: "https://images.pexels.com/photos/119342/pexels-photo-119342.jpeg?auto=compress&cs=tinysrgb&w=1600",
+                },
+                {
+                    id: 6,
+                    story: "https://images.pexels.com/photos/1087735/pexels-photo-1087735.jpeg?auto=compress&cs=tinysrgb&w=1600",
+                },
+                {
+                    id: 7,
+                    story: "https://images.pexels.com/photos/869258/pexels-photo-869258.jpeg?auto=compress&cs=tinysrgb&w=1600",
+                },
+                {
+                    id: 8,
+                    story: "https://images.pexels.com/photos/746386/pexels-photo-746386.jpeg?auto=compress&cs=tinysrgb&w=1600",
+                },
+            ],
+        },
+        {
+            id: 4,
+            title: "Water",
+            stories: [
+                {
+                    id: 1,
+                    story: "https://www.itsallbee.com/wp-content/uploads/2020/09/life-is-a-waterfall.jpg",
+                },
+                {
+                    id: 2,
+                    story: "https://www.itsallbee.com/wp-content/uploads/2020/09/waterfalls-quotes-love-640x960.jpg",
+                },
+                {
+                    id: 3,
+                    story: "https://assets.traveltriangle.com/blog/wp-content/uploads/2018/06/a-women-doing-yoga-in-front-of-a-waterfall-ss11052018.jpg",
+                },
+                {
+                    id: 4,
+                    story: "https://assets.traveltriangle.com/blog/wp-content/uploads/2018/06/a-women-walking-along-a-rainbow-waterfall-ss11052018.jpg",
+                },
+                {
+                    id: 5,
+                    story: "https://img.traveltriangle.com/blog/wp-content/uploads/2018/06/Tegenungan-Waterfall-cover.jpg",
+                },
+                {
+                    id: 6,
+                    story: "https://assets.traveltriangle.com/blog/wp-content/uploads/2018/06/a-rainbow-waterfall-amid-forests-ss11052018.jpg",
+                },
+            ],
+        },
+        {
+            id: 1,
+            title: "Food",
+            stories: [
+                {
+                    id: 1,
+                    story: "https://img.freepik.com/free-photo/top-view-food-frame-with-copy-space_23-2148723447.jpg?size=626&ext=jpg",
+                },
+                {
+                    id: 2,
+                    story: "https://img.freepik.com/free-photo/top-view-delicious-fish-meal-tray_23-2148734692.jpg?size=626&ext=jpg",
+                },
+                {
+                    id: 3,
+                    story: "https://t3.ftcdn.net/jpg/02/54/45/12/240_F_254451278_7AAYmhYkBEVQ9MYnZYIomTI5WdFCif1g.jpg",
+                },
+                {
+                    id: 4,
+                    story: "https://img.freepik.com/free-photo/indian-chicken-biryani-served-terracotta-bowl-with-yogurt-white-background-selective-focus_466689-72552.jpg?size=338&ext=jpg",
+                },
+                {
+                    id: 5,
+                    story: "https://img.freepik.com/free-photo/schezwan-noodles-szechwan-vegetable-hakka-noodles-chow-mein-is-popular-indo-chinese-recipes-served-bowl-plate-with-wooden-chopsticks_466689-74647.jpg?size=626&ext=jpg",
+                },
+                {
+                    id: 6,
+                    story: "https://img.freepik.com/free-photo/top-view-colorful-hummus_23-2149447785.jpg?size=338&ext=jpg",
+                },
+                {
+                    id: 7,
+                    story: "https://img.freepik.com/free-photo/aloo-paratha-gobi-paratha-also-known-as-potato-cauliflower-stuffed-flatbread-dish-originating-from-indian-subcontinent_466689-76173.jpg?size=626&ext=jpg",
+                },
+                {
+                    id: 8,
+                    story: "https://img.freepik.com/free-photo/misal-pav-is-popular-maharashtrian-street-food-usal-sprouts-curry-topped-with-onions-tomatoes-farsan-chutney_466689-74294.jpg?size=626&ext=jpg",
+                },
+            ],
+        },
+    ],
+    posts: [
+        {
+            id: 1,
+            location: "Peaceful mind",
+            likes: 70,
+            isLiked: true,
+            isArchived: false,
+            isSaved: true,
+            post: postOne,
+        },
+        {
+            id: 2,
+            location: "Munnar",
+            likes: 82,
+            isLiked: true,
+            isArchived: false,
+            isSaved: true,
+            post: "https://thumbs.dreamstime.com/b/web-166076793.jpg",
+        },
+        {
+            id: 3,
+            location: "Home",
+            likes: 67,
+            isLiked: false,
+            isArchived: false,
+            isSaved: false,
+            post: "https://thumbs.dreamstime.com/z/woman-praying-free-birds-to-nature-sunset-background-woman-praying-free-birds-enjoying-nature-sunset-99680945.jpg",
+        },
+        {
+            id: 4,
+            location: "Ilaveezhapoonchira",
+            likes: 58,
+            isLiked: false,
+            isArchived: false,
+            isSaved: false,
+            post: "https://thumbs.dreamstime.com/b/free-happy-woman-praising-freedom-beach-sunset-arms-up-young-adult-enjoying-breathing-freely-fresh-air-49157481.jpg",
+        },
+        {
+            id: 5,
+            location: "Ootty",
+            likes: 108,
+            isLiked: false,
+            isArchived: false,
+            isSaved: false,
+            post: "https://thumbs.dreamstime.com/b/free-thinking-nourish-your-mind-positive-thoughts-good-intentions-brain-power-concept-woman-head-silhouette-sunset-inside-157807698.jpg",
+        },
+        {
+            id: 6,
+            location: "Idukki",
+            likes: 99,
+            isLiked: false,
+            isArchived: false,
+            isSaved: false,
+            post: "https://thumbs.dreamstime.com/b/business-woman-relaxing-sitting-office-stress-free-time-management-concept-happy-young-her-65461487.jpg",
+        },
+        {
+            id: 7,
+            location: "Kodaikanal",
+            likes: 451,
+            isLiked: false,
+            isArchived: false,
+            isSaved: false,
+            post: "https://thumbs.dreamstime.com/b/free-range-chickens-farm-outdoor-91178223.jpg",
+        },
+        {
+            id: 8,
+            location: "Munnar",
+            likes: 321,
+            isLiked: false,
+            isArchived: false,
+            isSaved: false,
+            post: "https://thumbs.dreamstime.com/b/freedom-people-living-free-happy-life-beach-carefree-silhouettes-couple-sunset-arms-raised-up-showing-happiness-52124196.jpg",
+        },
+        {
+            id: 9,
+            location: "Thrissur",
+            likes: 202,
+            isLiked: false,
+            isArchived: false,
+            isSaved: false,
+            post: "https://thumbs.dreamstime.com/b/woman-feeling-free-beautiful-natural-setting-what-field-sunset-chains-175907923.jpg",
+        },
+        {
+            id: 10,
+            location: "Ponmudi",
+            likes: 458,
+            isLiked: false,
+            isArchived: false,
+            isSaved: false,
+            post: "https://thumbs.dreamstime.com/b/free-as-wind-5565107.jpg",
+        },
+        {
+            id: 11,
+            location: "Unknown",
+            likes: 745,
+            isLiked: false,
+            isArchived: false,
+            isSaved: false,
+            post: "https://thumbs.dreamstime.com/z/woman-praying-free-birds-to-nature-sunset-background-woman-praying-free-birds-enjoying-nature-sunset-99680945.jpg",
+        },
+    ],
+};
 
 export default userData;
